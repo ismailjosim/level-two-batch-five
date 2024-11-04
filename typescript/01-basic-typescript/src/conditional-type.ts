589fb8f46bd944b8b33bcaf5883cc31ab3a27d9e
ismailjosim <ismailjosim99@gmail.com>
2024-11-05 00:17:14 +0600

    conditional type

--- typescript/01-basic-typescript/src/conditional-type.ts
+++ typescript/01-basic-typescript/src/conditional-type.ts
@@ -15,4 +15,16 @@
 	let name1: TypeName<string> // 'string'
 	let name2: TypeName<number> // 'number'
 	let name3: TypeName<boolean> // 'object'
+
+	type Sheikh = {
+		bike: string
+		car: string
+		ship: string
+		plane: string
+	}
+
+	type CheckVehicle<T> = T extends keyof Sheikh ? true : false
+
+	type HasPlane = CheckVehicle<'plane'> // type = true
+	type HasOil = CheckVehicle<'oil'> // type = false
 }
