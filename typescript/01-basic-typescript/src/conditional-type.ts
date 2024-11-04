054d565c2a14f521a5706cff61164950af8ac133
ismailjosim <ismailjosim99@gmail.com>
2024-11-05 00:13:34 +0600

    conditional type

---
+++ typescript/01-basic-typescript/src/conditional-type.ts
@@ -0,0 +1,18 @@
+{
+	type a1 = number
+	type a2 = undefined
+
+	type x = a1 extends null ? true : false
+
+	type y = a1 extends null ? true : a2 extends undefined ? undefined : any
+
+	type TypeName<T> = T extends string
+		? 'string'
+		: T extends number
+		? 'number'
+		: 'object'
+
+	let name1: TypeName<string> // 'string'
+	let name2: TypeName<number> // 'number'
+	let name3: TypeName<boolean> // 'object'
+}
