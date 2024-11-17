cb3e39596a8b449d188d428ec3e2ddd9e71c676a
ismailjosim <ismailjosim99@gmail.com>
2024-11-17 23:12:48 +0600

    create interface for student

--- nodejs/university-server/eslint.config.mjs
+++ nodejs/university-server/eslint.config.mjs
@@ -1,19 +1,26 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 export default [
   {
-    files: ["**/*.{js,mjs,cjs,ts}"], // Define file patterns
+    files: ['**/*.{js,mjs,cjs,ts}'], // Define file patterns
     languageOptions: {
       globals: globals.browser, // Specify browser globals
     },
     ignores: [
-      "node_modules/**",
-      "dist/**", // Ignore common directories
+      'node_modules/**',
+      'dist/**', // Ignore common directories
     ],
     rules: {
-      "no-unused-vars": "error", // Example rule
+      'no-unused-vars': 'error', // Example rule
+      'no-unused-expressions': 'error',
+      'prefer-const': 'error',
+      'no-console': 'warn',
+      'no-undef': 'error',
+    },
+    globals: {
+      process: 'readonly',
     },
   },
   pluginJs.configs.recommended,
