51b5461f4a4644feffc764b614fd25ee864a09a1
ismailjosim <ismailjosim99@gmail.com>
2024-12-09 11:33:50 +0600

    crate all routes for user

--- nodejs/university-management-server/university-server-site/src/app/modules/user/user.controller.ts
+++ nodejs/university-management-server/university-server-site/src/app/modules/user/user.controller.ts
@@ -30,7 +30,86 @@ const createUser = async (req: Request, res: Response) => {
   }
 };
 
+const getAllUser = async (req: Request, res: Response) => {
+  try {
+    const result = await UserServices.getAllUserFromDB();
+    res.status(201).json({
+      success: true,
+      message: 'All Users Data Fetched Successfully!',
+      total: result.length,
+      result,
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({
+        success: false,
+        message: error.message || 'There Was An Error While Getting User.',
+      });
+    } else {
+      res.status(500).json({
+        status: false,
+        message: 'An Unexpected Error Occurred.',
+        error,
+      });
+    }
+  }
+};
+
+const getSingleUser = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const result = await UserServices.getSingleUserFromDB(id);
+    res.status(201).json({
+      success: true,
+      message: 'Found User with id ' + id,
+      data: result,
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      res.status(500).json({
+        success: false,
+        message: error.message || 'There was an error getting the User',
+        error,
+      });
+    } else {
+      res.status(500).json({
+        success: false,
+        message: 'An unexpected error occurred',
+        error,
+      });
+    }
+  }
+};
+const deleteSingleUser = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const result = await UserServices.deleteSingleUserFromDB(id);
+    res.status(201).json({
+      success: true,
+      message: 'User is Deleted successfully. ID: ' + id,
+      data: result,
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      res.status(500).json({
+        success: false,
+        message: error.message || 'There was an error creating the User',
+        error,
+      });
+    } else {
+      res.status(500).json({
+        success: false,
+        message: 'An unexpected error occurred',
+        error,
+      });
+    }
+  }
+};
+
 // export user controllers
 export const UserControllers = {
   createUser,
+  getAllUser,
+  getSingleUser,
+  deleteSingleUser,
 };
--- nodejs/university-management-server/university-server-site/src/app/modules/user/user.service.ts
+++ nodejs/university-management-server/university-server-site/src/app/modules/user/user.service.ts
@@ -6,7 +6,34 @@ const createUserIntoDB = async (useData: IUser) => {
   return res;
 };
 
+const getAllUserFromDB = async () => {
+  const res = await User.find();
+  return res;
+};
+
+const getSingleUserFromDB = async (id: string) => {
+  const res = await User.findById(id);
+  return res;
+};
+
+const deleteSingleUserFromDB = async (id: string) => {
+  const getUser = await getSingleUserFromDB(id);
+  if (getUser) {
+    if (getUser.isDeleted === false) {
+      const res = await User.updateOne({ _id: id }, { isDeleted: true });
+      return res;
+    } else {
+      throw new Error('User is Already Deleted!');
+    }
+  } else {
+    throw new Error('User Not Found!');
+  }
+};
+
 // export user
 export const UserServices = {
   createUserIntoDB,
+  getAllUserFromDB,
+  getSingleUserFromDB,
+  deleteSingleUserFromDB,
 };
