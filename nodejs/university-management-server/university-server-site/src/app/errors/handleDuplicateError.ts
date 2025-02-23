ff85f230996609e6dd2b4edc85125fa05afcffe4
ismailjosim <ismailjosim99@gmail.com>
2025-02-23 16:27:59 +0600

    add different types of error

---
+++ nodejs/university-management-server/university-server-site/src/app/errors/handleDuplicateError.ts
@@ -0,0 +1,28 @@
+import { TErrorSource, TGenericErrorResponse } from '../../interfaces/error';
+
+import { MongoError } from 'mongodb';
+
+const handleDuplicateError = (err: MongoError): TGenericErrorResponse => {
+  // Extract value within double quotes using regex
+  const match = err.message.match(/"([^"]*)"/);
+
+  // The extracted value will be in the first capturing group
+  const extractedMessage = match && match[1];
+
+  const errorSources: TErrorSource = [
+    {
+      path: '',
+      message: `${extractedMessage} is already exists`,
+    },
+  ];
+
+  const statusCode = 400;
+
+  return {
+    statusCode,
+    message: 'Invalid ID',
+    errorSources,
+  };
+};
+
+export default handleDuplicateError;
